4b1298b7c490d8ed637478c0c075131f3804e596
Jeroen Verkerk <jverkerk@alltrafficsolutions.com>
2020-04-16 11:58:26 -0400

    making test a bit nicer

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+printf "\n\n\n\n"
+printf "============================\n"
+printf "=    Site tests run here   =\n"
+printf "============================\n"
+printf "Test #1: Site connectivity\n"
 RESULT=$(curl -sI http://54.189.46.60/|awk 'FNR==1 {print $2}')
 if [ "$RESULT" == "200" ]; then
-  echo "site is alive"
+  printf "  Test #1 Passed: site is alive\n"
 else
-  echo "couldn't reach site"
-  #exit 1
+  printf "  Test #1 Failed: couldn't reach site\n"
+  exit 1
 fi
